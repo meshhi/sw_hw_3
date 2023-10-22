0e3701c82fe9137e3b933ade853a894623f17943
meshhi <yumythug@yandex.ru>
2023-10-22 12:17:34 +0300

    locators

--- src/test/java/github/meshhi/LoginPage.java
+++ src/test/java/github/meshhi/LoginPage.java
@@ -6,13 +6,28 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
+    public WebDriver driver;
     @FindBy(css = "#user-name")
     private WebElement loginField;
     @FindBy(css = "#password")
     private WebElement passwordField;
     @FindBy(css = "#login-button")
     private WebElement loginButton;
-    public WebDriver driver;
+    // 8 locators for purpose of homework 
+    @FindBy(id = "user-name")
+    private WebElement loginFieldById;
+    @FindBy(className = "submit-button")
+    private WebElement loginFieldByClassName;
+    @FindBy(tagName = "input")
+    private WebElement loginButtonByTagName; // incorrect, can`t identify DOM element by tag only
+    @FindBy(name = "user-name")
+    private WebElement loginButtonByName;
+    @FindBy(linkText = "Link-text example") 
+    private WebElement loginButtonByLinkText; // incorrect, no links on page
+    @FindBy(partialLinkText = "example") 
+    private WebElement loginButtonByPartialLinkText; // incorrect, no links on page
+    @FindBy(xpath = "//input[@id='user-name']")
+    private WebElement loginButtonByXPath;
 
     public LoginPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
